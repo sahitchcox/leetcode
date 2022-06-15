9ede9c05382efc901b7157170cd1c86363c09a3a
Sarah Hitchcox <sarahahitchcox@gmail.com>
2022-06-15 16:02:03 -0400

    Updates

---
+++ data/0020-valid-parentheses/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ const isValid = function(s) {
+  const stack = [];
+  const map = {
+  '(': ')',
+  '[': ']',
+  '{': '}'
+  };
+  for (let i = 0; i < s.length; i++) {
+      if (stack.length > 0 && map[stack[stack.length - 1]] === s[i]) {
+          stack.pop();
+      } else {
+          stack.push(s[i]);
+      }
+  }
+  return stack.length === 0;
+};
